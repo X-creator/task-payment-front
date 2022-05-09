cabfb3986f0504d359d41e5457493c796b9ef234
X-creator <farengeyt12@mail.ru>
2022-05-09 13:34:44 +1000

    abc

--- src/helpers/formatInputs.js
+++ src/helpers/formatInputs.js
@@ -4,15 +4,12 @@ export const formatCard = (value) => {
   return value.replace(/(\d{4})(?=\d)/g, "$1  ");
 };
 
-
 export const formatExpiration = (value) => value;
-
 
 export const formatCvv = (value) => {
   if (/\D/.test(value) || value.length > 3) return null;
   return value;
 };
-
 
 export const formatAmount = (value) => {
   value = value.replace(/,/g, "");
